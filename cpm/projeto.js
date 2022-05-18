f96a5322c4bd8df81ae5c1c5d17ac0bda96c704b
motadaniela <40210288@esmad.ipp.pt>
2022-05-18 15:36:34 +0100

    progresso

--- cpm/projeto.js
+++ cpm/projeto.js
@@ -9,9 +9,9 @@ let moto = document.querySelector("#moto_inteira");
 let carro = document.querySelector("#carro_todo");
 
 green.addEventListener('click',()=>{
-    carrinha.setAttribute("animation", `property: position; from: -12 -0.9 -7; to: 100 -0.9 -7; loop: false; delay: 300; dir: alternate; dur: 6000; easing: easeInOutCubic`)
-    moto.setAttribute("animation", `property: rotation; to: 0 100 0; loop: false; delay:1500; dur: 5000; easing: easeInOutCubic`)
-    carro.setAttribute("animation",`property: rotation; to: 0 90 0; loop: false; dur: 2000; delay:4500; easing: linear`)
+    carrinha.setAttribute("animation", `property: position; from: -12 -0.9 -7; to: 100 -0.9 -7; loop: false; delay: 300; dir: alternate; dur: 6000; easing: easeInOutCubic`);
+    moto.setAttribute("animation", `property: rotation; to: 0 100 0; loop: false; delay:1500; dur: 5000; easing: easeInOutCubic`);
+    carro.setAttribute("animation",`property: rotation; to: 0 90 0; loop: false; dur: 2000; delay:4500; easing: linear`);
 })
 red.addEventListener('click',()=>{
     carrinha.setAttribute("animation", `property: position; from: -12 -0.9 -7; to: 100 -0.9 -7; loop: false; delay: 300; dir: alternate; dur: 6000; easing: easeInOutCubic`)
@@ -19,10 +19,7 @@ red.addEventListener('click',()=>{
     carro.setAttribute("animation",`property: rotation; to: 0 90 0; loop: false; dur: 2000; delay:2000; easing: linear`)
 })
 yellow.addEventListener('click',()=>{
-    carrinha.setAttribute("animation", `property: position; from: -12 -0.9 -7; to: 100 -0.9 -7; loop: false; delay: 300; dir: alternate; dur: 6000; easing: easeInOutCubic`)
-    moto.setAttribute("animation", `property: rotation; to: 0 100 0; loop: false; dur: 5000; easing: easeInOutCubic`)
-    carro.setAttribute("animation",`property: rotation; to: 0 90 0; loop: false; dur: 2000; delay:2000; easing: linear`)
-})
+bts(carrinha,carro,moto)})
 blue.addEventListener('click', ()=>{
     carrinha.removeAttribute("animation");
     carro.removeAttribute("animation");
@@ -32,3 +29,17 @@ blue.addEventListener('click', ()=>{
     moto.setAttribute("rotation", '0 0 0');
 })
 /*carrinha-->moto-->carro*/
+
+
+async function bts (carrinha, carro, moto){
+    carrinha.setAttribute("animation", `property: position; from: -12 -0.9 -7; to: 100 -0.9 -7; loop: false; delay: 300; dir: alternate; dur: 6000; easing: easeInOutCubic`);
+    moto.setAttribute("animation", `property: rotation; to: 0 100 0; loop: false; delay:1500; dur: 5000; easing: easeInOutCubic`);
+    await sleep(500)
+    moto.setAttribute("animation", `property: position; to: 4 0 -100; loop: false; delay:1500; dur: 5000; easing: easeInOutCubic`);
+    carro.setAttribute("animation",`property: rotation; to: 0 90 0; loop: false; dur: 2000; delay:4500; easing: linear`);
+
+}
+
+function sleep(ms){
+    return new Promise(resolve => setTimeout(resolve, ms))
+}
