421244bae98f8d5fff66a2732ebba38068a4345d
motadaniela <40210288@esmad.ipp.pt>
2022-05-18 23:55:15 +0100

    :)

--- cpm/projeto.js
+++ cpm/projeto.js
@@ -9,9 +9,10 @@ let moto = document.querySelector("#moto_inteira");
 let carro = document.querySelector("#carro_todo");
 
 green.addEventListener('click',()=>{
-    carrinha.setAttribute("animation", `property: position; from: -15 3.8 -4; to: 100 3.8 -4; loop: false; delay: 300; dir: alternate; dur: 6000; easing: easeInOutCubic`)
-    moto.setAttribute("animation", `property: rotation; to: 0 100 0; loop: false; delay:1500; dur: 5000; easing: easeInOutCubic`)
-    carro.setAttribute("animation",`property: rotation; to: 0 90 0; loop: false; dur: 2000; delay:4500; easing: linear`)
+    carrinha.setAttribute("animation", `property: position; from: -15 3.8 -4; to: 100 3.8 -4; loop: false; delay: 300; dir: alternate; dur: 6000; easing: easeInOutCubic`);
+    moto.setAttribute("animation", `property: rotation; to: 0 100 0; loop: false; delay:1500; dur: 5000; easing: easeInOutCubic`);
+    carro.setAttribute("animation",`property: rotation; to: 0 90 0; loop: false; dur: 2000; delay:4500; easing: linear`);
+    correct(carrinha, carro, moto);
 })
 red.addEventListener('click',()=>{
     carrinha.setAttribute("animation", `property: position; from: -20.25605 6.59958 -2.19438; to: 100 6.59958 -2.19438; loop: false; delay: 300; dir: alternate; dur: 6000; easing: easeInOutCubic`)
@@ -34,12 +35,9 @@ blue.addEventListener('click', ()=>{
 /*carrinha-->moto-->carro*/
 
 
-async function bts (carrinha, carro, moto){
-    carrinha.setAttribute("animation", `property: position; from: -12 -0.9 -7; to: 100 -0.9 -7; loop: false; delay: 300; dir: alternate; dur: 6000; easing: easeInOutCubic`);
-    moto.setAttribute("animation", `property: rotation; to: 0 100 0; loop: false; delay:1500; dur: 5000; easing: easeInOutCubic`);
-    carro.setAttribute("animation",`property: rotation; to: 0 90 0; loop: false; dur: 2000; delay:4500; easing: linear`);
-    await sleep(500)
-    moto.setAttribute("animation", `property: position; to: 4 0 -100; loop: false; delay:1500; dur: 5000; easing: easeInOutCubic`);
+async function correct (carrinha, carro, moto){
+    await sleep(200)
+    moto.setAttribute("animation", `property: position; from:4 0 -3.7; to: -100 0 -3.7; loop: false; delay:1500; dur: 5000; easing: easeInOutCubic`);
     carro.setAttribute("animation",`property: rotation; to: 0 90 0; loop: false; dur: 2000; delay:4500; easing: linear`);
 
 }
