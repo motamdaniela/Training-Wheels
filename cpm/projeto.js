e840f50030cf3d9ad9a91d4bd762135315b84e2f
beatriz rodrigues <40210313@esmad.ipp.pt>
2022-05-18 15:05:25 +0100

    uwu

--- cpm/projeto.js
+++ cpm/projeto.js
@@ -10,8 +10,8 @@ let carro = document.querySelector("#carro_todo");
 
 green.addEventListener('click',()=>{
     carrinha.setAttribute("animation", `property: position; from: -12 -0.9 -7; to: 100 -0.9 -7; loop: false; delay: 300; dir: alternate; dur: 6000; easing: easeInOutCubic`)
-    moto.setAttribute("animation", `property: rotation; to: 0 100 0; loop: false; delay:2000; dur: 5000; easing: easeInOutCubic`)
-    carro.setAttribute("animation",`property: rotation; to: 0 90 0; loop: false; dur: 2000; delay:6000; easing: linear`)
+    moto.setAttribute("animation", `property: rotation; to: 0 100 0; loop: false; delay:1500; dur: 5000; easing: easeInOutCubic`)
+    carro.setAttribute("animation",`property: rotation; to: 0 90 0; loop: false; dur: 2000; delay:4500; easing: linear`)
 })
 red.addEventListener('click',()=>{
     carrinha.setAttribute("animation", `property: position; from: -12 -0.9 -7; to: 100 -0.9 -7; loop: false; delay: 300; dir: alternate; dur: 6000; easing: easeInOutCubic`)
