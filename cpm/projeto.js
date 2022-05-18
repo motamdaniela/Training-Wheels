f417c6590f14e19f1f0bb6da760da3d97edc9696
motadaniela <40210288@esmad.ipp.pt>
2022-05-18 14:27:28 +0100

    botao reset em progresso

--- cpm/projeto.js
+++ cpm/projeto.js
@@ -1,30 +1,34 @@
 let green = document.querySelector("#btn_green");
 let red = document.querySelector("#btn_red");
 let yellow = document.querySelector("#btn_yellow");
+let blue = document.querySelector("#btn_blue");
+
+/*veiculos*/
+let carrinha = document.querySelector("#carrinha");
+let moto = document.querySelector("#moto_inteira");
+let carro = document.querySelector("#carro_todo");
+
+/*posicoes iniciais*/
+let moto_position = '';
 
 green.addEventListener('click',()=>{
-    let carrinha = document.querySelector("#carrinha");
-    let moto = document.querySelector("#moto_inteira");
-    let carro = document.querySelector("#carro_todo");
     carrinha.setAttribute("animation", `property: position; from: -12 -0.9 -7; to: 100 -0.9 -7; loop: false; delay: 300; dir: alternate; dur: 6000; easing: easeInOutCubic`)
     moto.setAttribute("animation", `property: rotation; to: 0 100 0; loop: false; delay:2000; dur: 5000; easing: easeInOutCubic`)
     carro.setAttribute("animation",`property: rotation; to: 0 90 0; loop: false; dur: 2000; delay:6000; easing: linear`)
 })
 red.addEventListener('click',()=>{
-    let carrinha = document.querySelector("#carrinha");
-    let moto = document.querySelector("#moto_inteira");
-    let carro = document.querySelector("#carro_todo");
     carrinha.setAttribute("animation", `property: position; from: -12 -0.9 -7; to: 100 -0.9 -7; loop: false; delay: 300; dir: alternate; dur: 6000; easing: easeInOutCubic`)
     moto.setAttribute("animation", `property: rotation; to: 0 100 0; loop: false; dur: 5000; easing: easeInOutCubic`)
     carro.setAttribute("animation",`property: rotation; to: 0 90 0; loop: false; dur: 2000; delay:2000; easing: linear`)
 })
 yellow.addEventListener('click',()=>{
-    let carrinha = document.querySelector("#carrinha");
-    let moto = document.querySelector("#moto_inteira");
-    let carro = document.querySelector("#carro_todo");
     carrinha.setAttribute("animation", `property: position; from: -12 -0.9 -7; to: 100 -0.9 -7; loop: false; delay: 300; dir: alternate; dur: 6000; easing: easeInOutCubic`)
     moto.setAttribute("animation", `property: rotation; to: 0 100 0; loop: false; dur: 5000; easing: easeInOutCubic`)
     carro.setAttribute("animation",`property: rotation; to: 0 90 0; loop: false; dur: 2000; delay:2000; easing: linear`)
 })
-
+blue.addEventListener('click', ()=>{
+    carrinha.removeAttribute("animation");
+    carro.setAttribute("animation");
+    moto.setAttribute("animation");
+})
 /*carrinha-->moto-->carro*/
