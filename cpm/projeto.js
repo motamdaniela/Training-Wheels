721678cc3b6f232c3c84743922c447df0b8c2631
beatriz rodrigues <40210313@esmad.ipp.pt>
2022-05-19 16:26:05 +0100

    uwu

--- cpm/projeto.js
+++ cpm/projeto.js
@@ -8,6 +8,9 @@ let carrinha = document.querySelector("#truck");
 let moto = document.querySelector("#moto_inteira");
 let carro = document.querySelector("#carro_todo");
 
+// fogo
+let fogo=document.querySelector("#explosao")
+
 green.addEventListener('click',()=>{
     correct(carrinha, carro, moto)
 })
@@ -28,19 +31,21 @@ reset.addEventListener('click', ()=>{
     carrinha.setAttribute("position",'-20.420 3.8 -4');
     carro.setAttribute("position",'-9 0 0');
     moto.setAttribute("position",'6 0 -5');
+    fogo.setAttribute("visible",'false');
 })
 
 /*movimentos da opcao correta*/
-async function correct (carrinha, carro, moto){
+async function correct (carrinha, carro, moto,fogo){
     carrinha.setAttribute("animation", `property: position; from: -20.420 3.8 -4; to: 100 3.8 -4; loop: false; delay: 300; dir: alternate; dur: 6000; easing: easeInOutCubic`)
     moto.setAttribute("animation", `property: rotation; to: 0 100 0; loop: false; delay:1500; dur: 3500; easing: easeInOutCubic`)
     await sleep(4000)
     moto.setAttribute("animation", `property: position; from:6 0 -5; to: 4 0 100; loop: false; delay:50; dur: 5000; easing: linear`);
-    carro.setAttribute("animation",`property: position; from:-9 0 0; to: -9 0 -6; loop: false; dur: 500; delay:500; easing: linear`);
+    carro.setAttribute("animation",`property: position; from:-9 0 0; to: -9 0 -6; loop: false; dur: 500; delay:500; easing: linear`,`property: rotation; to: 0 90 0; loop: false; dur: 1500; delay:50; easing: easeInOutCubic`);
     await sleep(1000)
-    carro.setAttribute("animation",`property: rotation; to: 0 90 0; loop: false; dur: 1500; delay:50; easing: easeInOutCubic`)
+    carro.setAttribute("animation",`property: rotation; from:0 0 0; to: 0 90 0; loop: false; dur: 1500; delay:50; easing: easeInOutCubic`)
     await sleep(1500)
-    carro.setAttribute("animation",`property: position; from:-9 0 -6; to: -100 0 -3; loop: false; dur: 2000; delay:500; easing: linear`);
+    carro.setAttribute("animation",`property: position; from:-9 0 -6; to: -100 0 -3; loop: false; dur: 2000; delay:50; easing: linear`);
+    
 
 }
 
@@ -50,12 +55,18 @@ async function wrong_moto (carrinha, carro, moto){
     await sleep(2000)
     carrinha.setAttribute("animation", `property: position; from: -20.420 3.8 -4; to: -5 3.8 -4; loop: false; delay: 1000; dir: alternate; dur: 500; easing: easeInOutCubic`)
     carro.setAttribute("animation",`property: position; from:-9 0 0; to: -9 0 -8; loop: false; dur: 500; delay:700; easing: linear`);
+    fogo.setAttribute("animation",`property:position; from:-2.420 -1.2 -4; to:-2.420 -1 -4; delay:99999`)
+    await sleep(1500)
+    fogo.setAttribute("visible",`true`);
 }
 /*movimentos opcao errada em que o carro avança primeiro*/
 async function wrong_car (carrinha, carro, moto){
     carro.setAttribute("animation",`property: position; from:-9 0 0; to: -9 0 -10; loop: false; dur: 500; delay:200; easing: linear`);
     carrinha.setAttribute("animation", `property: position; from: -20.420 3.8 -4; to: -5 3.8 -4; loop: false; delay: 600; dir: alternate; dur: 500; easing: easeInOutCubic`)
     moto.setAttribute("animation", `property: rotation; to: 0 80 0; loop: false; delay:300; dur: 1000; easing: easeInOutCubic`)
+    fogo.setAttribute("animation",`property:position; from:-2.420 -1.2 -4; to:-2.420 -1 -4; delay:99999`)
+    await sleep(1200)
+    fogo.setAttribute("visible",`true`);
 }
 
 function sleep(ms){
