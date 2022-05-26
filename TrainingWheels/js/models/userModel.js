20e1f6eda405f98ea5d7cbaa6229035627f52499
Daniela Moreira <40210349@esmad.ipp.pt>
2022-05-26 16:47:26 +0100

    ^-^

---
+++ TrainingWheels/js/models/userModel.js
@@ -0,0 +1,36 @@
+export default class User {
+    #name = ''
+    #username = ''
+    #password = ''
+    #email = ''
+    #sex = ''
+    #birthday = '00-00-0000'
+    #place = ''
+    #photo = ''
+    #ranking = 0 
+    #type = ''
+    #progress = 0
+    #points = ''
+    #clues = 0
+    #stickers = []
+
+    constructor(name, username, password, email, sex, birthday, place, photo, ranking, type, progress, points,clues, stickers){
+        this.name = name;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.sex = sex;
+        this.birthday = birthday;
+        this.place = place;
+        this.photo = photo;
+        this.ranking = ranking;
+        this.type = type;
+        this.progress = progress;
+        this.points = points;
+        this.clues = clues;
+        this.stickers = stickers;
+
+        
+
+    }
+}
