10460262254d8dd10cee1942ab57f81519e42ef4
Daniela Moreira <40210349@esmad.ipp.pt>
2022-06-01 14:38:11 +0100

    :P

--- TrainingWheels/js/models/videoModel.js
+++ TrainingWheels/js/models/videoModel.js
@@ -13,4 +13,23 @@ export default class Video{
         this.#comments = comments;
         this.#level = level;
     }
+
+    get getUrl () { return this.#url }
+    set setUrl (value) { this.#url = value}
+
+    get getTags () { return this.#tags }
+    set setTags (value) { this.#tags = value}
+
+    get getName () { return this.#name }
+    set setName (value) {this.#name = value}
+
+    get getLikes () {return this.#likes}
+    set setLikes (value) {this.#likes = value}
+
+    get getComments () { return this.#comments}
+    set setComments (value) {this.#comments = value}
+
+    get getLevel () {return this.#level}
+    set setLevel (value) {this.#level = value}
+
 }
