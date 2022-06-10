c9b8d778d5beae0c75caeab557f0431885bc5745
motadaniela <40210288@esmad.ipp.pt>
2022-06-10 20:53:47 +0100

    pop up progresso:)

--- TrainingWheels/js/views/tutorialView.js
+++ TrainingWheels/js/views/tutorialView.js
@@ -30,9 +30,7 @@ for (const tagBtn of tagBtns){
 }
 
 document.querySelector('#modalBtn').addEventListener('click',()=>{
-    $('#myModal').on('shown.bs.modal', function () {
-        $('#myInput').trigger('focus')
-      })
+    $("#myModal").modal()
 })
 
 function PopUp(){
@@ -79,65 +77,4 @@ function PopUp(){
   
     </div>
   </div>
-*/
-
-
-//------------------
-
-// MANIPULAÇÃO DE BOTÕES
-document.querySelector('#btnDuration').addEventListener('click', () => {
-    duration.innerHTML = video.duration
-})
-
-document.querySelector('#btnPlay').addEventListener('click', () => {
-    video.play()
-})
-
-document.querySelector('#btnPause').addEventListener('click', () => {
-    video.pause()
-})
-
-document.querySelector('#btnGotoStart').addEventListener('click', () => {
-    video.currentTime = 0
-    video.play()
-})
-
-document.querySelector('#btnGotoEnd').addEventListener('click', () => {
-    video.currentTime = video.duration - 1
-})
-
-document.querySelector('#btnGotoFrame').addEventListener('click', () => {
-    video.currentTime = +prompt("Qual a frame?")
-})
-
-
-document.querySelector('#btnSave').addEventListener('click', () => {
-    const txtAnnotation = document.querySelector('#txtAnnotation').value
-
-    const annotationObject = {
-        frame: Math.trunc(video.currentTime),
-        text: txtAnnotation
-    }
-
-    annotations.push(annotationObject)
-    localStorage.setItem('annotations', JSON.stringify(annotations))
-})
-
-// EVENTOS DO VIDEO
-video.addEventListener('volumechange', () => {
-    if (video.volume > 0.5) {
-        alert(`Be carefull with your ears! The current volume is ${video.volume}!`)
-    }
-})
-
-video.addEventListener('ended', () => {
-    alert(`Thanks for watching this video!`)
-})
-
-/*
-video.addEventListener('timeupdate', () => {
-    if (Math.trunc(video.currentTime) == annotations[0].frame) {
-        alert(annotations[0].text)
-    }
-})
 */
