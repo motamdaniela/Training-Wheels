c60159a00a8f91df723c06c2f4aedde2ba9b64dc
motadaniela <40210288@esmad.ipp.pt>
2022-06-14 16:52:31 +0100

    Revert "Co-authored-by: Daniela <motadaniela@users.noreply.github.com>"
    This reverts commit 89df32d2bc55ee06b797447806fc93ed678a5aa1.

--- TrainingWheels/js/models/popupModel.js
+++ TrainingWheels/js/models/popupModel.js
@@ -1,51 +1,24 @@
 export default class popUp{
-    #question =""
-    #image =""
-    #answers =[]
-    #correctAnswer =""
-    #reward =""
-    #level =0
-    #video =""
-    #tag =""
-    #pointsEarned =0
+    question =""
+    image =""
+    answers =[]
+    correctAnswer =""
+    reward =""
+    level =0
+    video =""
+    tag =""
+    pointsEarned =0
 
     constructor(question, image, answer, correctAnswer, reward, level, video, tag, pointsEarned) {
-        this.#question = question;
-        this.#image = image;
-        this.#answers = answers;
-        this.#correctAnswer = correctAnswer;
-        this.#reward = reward;
-        this.#level = level;
-        this.#video = video;
-        this.#tag = tag;
-        this.#pointsEarned = pointsEarned;
+        this.question = question;
+        this.image = image;
+        this.answers = answers;
+        this.correctAnswer = correctAnswer;
+        this.reward = reward;
+        this.level = level;
+        this.video = video;
+        this.tag = tag;
+        this.pointsEarned = pointsEarned;
     }
-
-    get getQuestion () { return this.#question}
-    set setQuestion (value) { this.#question = value}
-
-    get getImage () { return this.#image}
-    set setImage (value) {this.#image = value}
-
-    get getAnswers () { return this.#answers}
-    set setAnswers (value) { this.#answers = value}
-
-    get getCorrectAnswer () { return this.#correctAnswer}
-    set setCorrectAnswer (value) { this.#correctAnswer = value}
-
-    get getReward () { return this.#reward}
-    set setReward (value) { this.#reward = value}
-
-    get getLevel () { return this.#level}
-    set setLevel (value) { this.#level = value}
-
-    get getVideo () { return this.#video}
-    set setVideo (value) { this.#video = value}
-
-    get getTag () { return this.#tag}
-    set setTag (value) { this.#tag = value}
-
-    get getPointsEarned () { return this.#pointsEarned}
-    set setPointsEarned (value) { this.#pointsEarned = value}
 
 }
