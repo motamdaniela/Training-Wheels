66ea58d8bdd33f90115df7647bc3d02c6132b860
motadaniela <40210288@esmad.ipp.pt>
2022-06-16 01:00:47 +0100

    feeling quirky faq done(with humor -v-)

--- TrainingWheels/js/models/progressModel.js
+++ TrainingWheels/js/models/progressModel.js
@@ -2,15 +2,17 @@ class Progress{
     username = ''
     points = 0
     questionsDone = []
+    questionsCorrect = []
     levelsDone = []
     currentVideo = {}
     currentTag = ''
     likedVideos = []
 
-    constructor(username, points = 0, questionsDone = [], levelsDone = [], currentVideo = {}, currentTag = '', likedVideos = []) {
+    constructor(username, points = 0, questionsDone = [], questionsCorrect = [], levelsDone = [], currentVideo = {}, currentTag = '', likedVideos = []) {
         this.username = username
         this.points = points
         this.questionsDone = questionsDone
+        this.questionsCorrect = questionsCorrect
         this.levelsDone = levelsDone
         this.currentVideo = currentVideo
         this.currentTag = currentTag
