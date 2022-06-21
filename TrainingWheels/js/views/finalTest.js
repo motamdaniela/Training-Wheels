7b3a395f06546859307104d00949a37f1d6ed57c
motadaniela <40210288@esmad.ipp.pt>
2022-06-21 18:57:04 +0100

    slayyyyyyyyy

--- TrainingWheels/js/views/finalTest.js
+++ TrainingWheels/js/views/finalTest.js
@@ -34,37 +34,94 @@ function renderPage(){
             if(pergunta.test_name === teste.name){
 
               string += `
-              <fieldset class="accordion blue fldfld">
+              <fieldset class="accordion blue fldfld" id="${pergunta.question}">
               <div class="title4 lvlTitle">
                   <i class="nav-icon4" data-feather="circle"></i> 
-                  <h3>${pergunta.question}</h3>
+                  <h3 class="perguntaTitle">${pergunta.question}</h3>
               </div>
 
                     <div class="row toPad" id="subQuestion">`
 
                     pergunta.answers.forEach((answer)=>{
                       string += `
-                      <button type="button" class="btn btn-primary answerBtn">${answer}</button>`
+                      <button id="${pergunta.question}" type="button" class="btn btn-primary answerBtn">${answer}</button>`
                     })
 
                     string +=`
                     </div></fieldset>
                     `
-            }
-          })
+                    
+                    
+                  }
+                })
+                
+              }
+            })
+            
+            document.querySelector('#divPages').innerHTML = string
 
+            let answerBtns = document.querySelectorAll('.answerBtn')
+            answerBtns = Array.from(answerBtns)
+
+            answerBtns.forEach((answerBtn)=>{
+              answerBtn.addEventListener('click',()=>{
+                lookFor(answerBtn)
+              })
+
+            })
+            
+            break;
+          }
         }
-      })
+      }
+      renderPage()
+      
 
-      document.querySelector('#divPages').innerHTML = string
+  
+  
+  function correctAnswer(questionObj, theFld, answerBtn){
+      
+      if(answerBtn.innerHTML === questionObj.right_answer){
+        console.log(questionObj.right_answer, answerBtn.innerHTML)
+          
+        currentUser.points += questionObj.points
+        sessionStorage.setItem('loggedUser', JSON.stringify(currentUser))
+        User.attUserOnStorage(currentUser)
+        
+        theFld.classList.remove('blue')
+        theFld.classList.add('green')
+        theFld.innerHTML = `<img src="../media/images/point.svg" height="20px"> + ${questionObj.points}`
+        
+      }else{
+        theFld.classList.remove('blue')
+        theFld.classList.add('red')
+        theFld.innerHTML = `Resposta errada! <img src="../media/images/sadgilf.svg" height="20px">`
+      }
 
-      break;
-    }
   }
-}
 
-renderPage()
+  function lookFor(answerBtn){
+    let flds = document.querySelectorAll('.fldfld')
+    flds = Array.from(flds)
 
-function correctAnswer(){
-  let answerBtns = document.querySelector('.answerBtn')
-}
+
+    flds.forEach((fld)=>{
+
+        if(answerBtn.id == fld.id){
+          perguntas.forEach((pergunta)=>{
+            if(pergunta.question === fld.id){
+
+              console.log(fld)
+              correctAnswer(pergunta, fld, answerBtn)
+            }
+  
+          })
+        }
+
+    })
+
+
+  }
+
+  feather.replace()
+  
